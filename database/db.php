43959258f65d5b6557a13ddba2e9caee4fbbed90
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-03-29 21:22:53 +0430

    add partials part to post page

--- database/db.php
+++ database/db.php
@@ -4,7 +4,7 @@
     define("DB_USER",'root');
     define("DB_PASSWORD",'');
     define("DB_NAME",'edwin-cms');
-    
+
     $connection = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die(mysqli_error($connection));
 
 ?>
