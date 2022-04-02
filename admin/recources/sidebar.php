5216c0da0900df85224a8216a4c9edd8c7194155
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-02 22:52:35 +0430

    refuctore the navbar and sidebar codes

--- admin/recources/sidebar.php
+++ admin/recources/sidebar.php
@@ -4,24 +4,10 @@
             <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
         </li>
         <li>
-            <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
-        </li>
-        <li>
-            <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
-        </li>
-        <li>
-            <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>
-        </li>
-        <li>
-            <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
-        </li>
-        <li>
-            <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
-        </li>
-        <li>
-            <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i>
-                Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
-            <ul id="demo" class="collapse">
+            <a href="javascript:;" data-toggle="collapse" data-target="#posts_dropdown"><i
+                    class="fa fa-fw fa-newspaper-o"></i>
+                Posts <b class="caret"></b></a>
+            <ul id="posts_dropdown" class="collapse">
                 <li>
                     <a href="#">Dropdown Item</a>
                 </li>
@@ -31,10 +17,26 @@
             </ul>
         </li>
         <li class="active">
-            <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
+            <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Categories</a>
+        </li>
+        <li class="active">
+            <a href="blank-page.html"><i class="fa fa-fw fa-comment-o"></i> Comments</a>
         </li>
         <li>
-            <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
+            <a href="javascript:;" data-toggle="collapse" data-target="#users_dropdown"><i
+                    class="fa fa-fw fa-newspaper-o"></i>
+                Users <b class="caret"></b></a>
+            <ul id="users_dropdown" class="collapse">
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+                <li>
+                    <a href="#">Dropdown Item</a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a href="index-rtl.html"><i class="fa fa-fw fa-user"></i> Profile</a>
         </li>
     </ul>
 </div>
