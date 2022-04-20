b212905e7dd9bb8fc0bf041fc915fd96e5cc75ea
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-20 23:21:32 +0430

    call saveComment function and insert comment

--- resources/partials/posts/single_post.php
+++ resources/partials/posts/single_post.php
@@ -1,120 +1,127 @@
-<?php               
+    <?php               
         // Edit post func -->
         if (isset($_GET["post_id"])){
             $data = getPostById($_GET["post_id"]);
-        }?>
-<div class="row">
+        }
+        if (isset($_POST['submit'])) {
+            $result = saveComment();
+            if (!$result == 1) {
+                echo $result;
+            }
+        }
+    ?>
+    <div class="row">
 
-    <!-- Blog Post Content Column -->
-    <div class="col-lg-8">
+        <!-- Blog Post Content Column -->
+        <div class="col-lg-8">
 
-        <!-- Blog Post -->
+            <!-- Blog Post -->
 
-        <!-- Title -->
-        <h1><?=$data['title']?></h1>
+            <!-- Title -->
+            <h1><?=$data['title']?></h1>
 
-        <!-- Author -->
-        <p class="lead">
-            by <a href="#"><?=$data['author']?></a>
-        </p>
+            <!-- Author -->
+            <p class="lead">
+                by <a href="#"><?=$data['author']?></a>
+            </p>
 
-        <hr>
+            <hr>
 
-        <!-- Date/Time -->
-        <p><span class="glyphicon glyphicon-time"></span> Posted on <?=$data['date']?></p>
+            <!-- Date/Time -->
+            <p><span class="glyphicon glyphicon-time"></span> Posted on <?=$data['date']?></p>
 
-        <hr>
+            <hr>
 
-        <!-- Preview Image -->
-        <?php $path = ($data['old_image'])? "./public/image/".$datas['old_image'] : "./public/image/image_1.jpg";?>
-        <img class="img-responsive" src="<?=$path?>" alt="<?=$data['title']?>">
+            <!-- Preview Image -->
+            <?php $path = ($data['old_image'])? "./public/image/".$datas['old_image'] : "./public/image/image_1.jpg";?>
+            <img class="img-responsive" src="<?=$path?>" alt="<?=$data['title']?>">
 
-        <hr>
+            <hr>
 
-        <!-- Post Content -->
-        <p class="lead">
-            <?= $data['content'] ?>
-        </p>
+            <!-- Post Content -->
+            <p class="lead">
+                <?= $data['content'] ?>
+            </p>
 
-        <hr>
+            <hr>
 
-        <!-- Blog Comments -->
+            <!-- Blog Comments -->
 
-        <!-- Comments Form -->
-        <div class="well">
-            <h4>Leave a Comment:</h4>
-            <form role="form">
-                <div class="form-group">
-                    <label for="author">Name</label>
-                    <input type="text" name="author" class="form-control" id="author">
-                </div>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" name="email" class="form-control" id="email">
-                </div>
-                <div class="form-group">
-                    <label for="content">Comment</label>
-                    <textarea name="content" class="form-control" rows="3" id="content"></textarea>
-                </div>
-                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-            </form>
-        </div>
-
-        <hr>
-
-        <!-- Posted Comments -->
-
-        <!-- Comment -->
-        <div class="media">
-            <a class="pull-left" href="#">
-                <img class="media-object" src="./public/image/user.png" alt="">
-            </a>
-            <div class="media-body">
-                <h4 class="media-heading">Start Bootstrap
-                    <small>August 25, 2014 at 9:30 PM</small>
-                </h4>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
-                commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce
-                condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            </div>
-        </div>
-
-        <!-- Comment -->
-        <div class="media">
-            <a class="pull-left" href="#">
-                <img class="media-object" src="./public/image/user.png" alt="">
-            </a>
-            <div class="media-body">
-                <h4 class="media-heading">Start Bootstrap
-                    <small>August 25, 2014 at 9:30 PM</small>
-                </h4>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
-                commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce
-                condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-                <!-- Nested Comment -->
-                <div class="media">
-                    <a class="pull-left" href="#">
-                        <img class="media-object" src="./public/image/user.png" alt="">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Nested Start Bootstrap
-                            <small>August 25, 2014 at 9:30 PM</small>
-                        </h4>
-                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-                        sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra
-                        turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
-                        felis in faucibus.
+            <!-- Comments Form -->
+            <div class="well">
+                <h4>Leave a Comment:</h4>
+                <form action="" method="POST" role="form">
+                    <div class="form-group">
+                        <label for="author">Name</label>
+                        <input type="text" name="author" class="form-control" id="author">
                     </div>
-                </div>
-                <!-- End Nested Comment -->
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control" id="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="content">Comment</label>
+                        <textarea name="content" class="form-control" rows="3" id="content"></textarea>
+                    </div>
+                    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                </form>
             </div>
+
+            <hr>
+
+            <!-- Posted Comments -->
+
+            <!-- Comment -->
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img class="media-object" src="./public/image/user.png" alt="">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading">Start Bootstrap
+                        <small>August 25, 2014 at 9:30 PM</small>
+                    </h4>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
+                    commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce
+                    condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                </div>
+            </div>
+
+            <!-- Comment -->
+            <div class="media">
+                <a class="pull-left" href="#">
+                    <img class="media-object" src="./public/image/user.png" alt="">
+                </a>
+                <div class="media-body">
+                    <h4 class="media-heading">Start Bootstrap
+                        <small>August 25, 2014 at 9:30 PM</small>
+                    </h4>
+                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin
+                    commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce
+                    condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    <!-- Nested Comment -->
+                    <div class="media">
+                        <a class="pull-left" href="#">
+                            <img class="media-object" src="./public/image/user.png" alt="">
+                        </a>
+                        <div class="media-body">
+                            <h4 class="media-heading">Nested Start Bootstrap
+                                <small>August 25, 2014 at 9:30 PM</small>
+                            </h4>
+                            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
+                            sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra
+                            turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
+                            felis in faucibus.
+                        </div>
+                    </div>
+                    <!-- End Nested Comment -->
+                </div>
+            </div>
+
         </div>
+
+
+        <!-- Blog Sidebar Widgets Column -->
+        <?php include_once("resources/layouts/sidebar.php");?>
 
     </div>
-
-
-    <!-- Blog Sidebar Widgets Column -->
-    <?php include_once("resources/layouts/sidebar.php");?>
-
-</div>
-<!-- /.row -->
+    <!-- /.row -->
