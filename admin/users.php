2f965f22b084ad0db23b63593ada75c18443f5b7
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-21 20:17:38 +0430

    create users page

---
+++ admin/users.php
@@ -0,0 +1,40 @@
+<?php include "recources/header.php";?>
+<?php include "function.php";?>
+<div id="wrapper">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <?php include_once "recources/navbar.php";?>
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+
+        <?php include_once "recources/sidebar.php";?>
+        <!-- /.navbar-collapse -->
+    </nav>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+            <?php 
+                $flag = (isset($_GET['flag']))? $_GET['flag'] : "";
+                switch ($flag) {
+                    case 'add':
+                        include "recources/partials/users/add-form.php";
+                        break;
+                    case 'edit':
+                        include "recources/partials/users/edit-form.php";
+                        break;
+                    default:
+                        include "recources/partials/users/users_table.php";
+                        break;
+                }
+            ?>
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+</div>
+<!-- /#wrapper -->
+
+<?php include_once "recources/footer.php";?>
