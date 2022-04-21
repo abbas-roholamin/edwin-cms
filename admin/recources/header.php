d2e91076021aa4e597ad5b99b488e99073f328e9
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-21 20:48:24 +0430

    add custome css file link in haeder

--- admin/recources/header.php
+++ admin/recources/header.php
@@ -18,6 +18,7 @@
 
         <!-- Custom CSS -->
         <link href="css/sb-admin.css" rel="stylesheet">
+        <link href="css/custome.css" rel="stylesheet">
 
         <!-- Custom Fonts -->
         <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
