3013a0ca4ab3ddd420d9a5f0ee12bb6211ef3c4b
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-21 20:18:09 +0430

    create user partials page

---
+++ admin/recources/partials/users/edit-form.php
@@ -0,0 +1,108 @@
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-lg-12">
+        <h2 class="page-header">
+            Dashboard / Edit Post
+        </h2>
+    </div>
+    <main>
+        <div class="col-lg-12">
+            <?php
+                // Edit post func -->
+                if (isset($_GET["id"])){
+                    $data = getPostById($_GET["id"]);
+                }
+
+                if (isset($_POST['upadate'])) {
+                    $id = $_POST['id'];
+                    if($id){
+                        $result = updatePost($id,$_POST);
+                        if (!$result == 1) {
+                            echo $result;
+                        }
+                    }
+                }
+            ?>
+            <form action="#" enctype="multipart/form-data" method="POST">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input type="hidden" name="id" value="<?=$data['id']?>">
+                            <input type="text" name="title" value="<?=$data['title']?>" class="form-control"
+                                placeholder="Post title" id="title">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+
+                        <div class="form-group">
+                            <label for="author">Author</label>
+                            <input type="text" name="author" value="<?=$data['author']?>" class="form-control"
+                                placeholder="Post author" id="author">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="category">category</label>
+                            <select class="form-control" id="category" name="category">
+                                <?php
+                                    // Get all categories form DB 
+                                   $rows = getAllCategories();
+                                    foreach ($rows as $row):
+                                        $id = $row['id'];
+                                        $title = $row['title'];
+                                ?>
+                                <option value=" <?= $id?>" <?=($id == $data['category'] )? "selected" : "";?>>
+                                    <?=$title ?>
+                                </option>
+                                <?php endforeach?>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="status">Status</label>
+                            <input type="text" name="status" value="<?=$data['status']?>" class="form-control"
+                                placeholder="Post status" id="status">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="form-group">
+                            <label for="tag">Tags</label>
+                            <input type="text" name="tags" value="<?=$data['tags']?>" class="form-control"
+                                placeholder="Post Tag" id="tag">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <!-- <?php if($data['old_image']):?>
+                        <img src="./public/image/<?=$data['old_image']?>" alt="old_image"
+                            style="max-width: 10rem; max-height: 10rem;">
+                        <?php endif?> -->
+                        <div class="custom-file form-group">
+                            <label for="tag">Image</label>
+                            <input type="hidden" value="<?=$data['old_image']?>" class="custom-file-input form-control"
+                                name="old_image" id="image">
+                            <input type="file" class="custom-file-input form-control" name="image" id="image">
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="form-group">
+                            <label for="content">Content</label>
+                            <textarea class="form-control" name="content" rows="3"
+                                id="content"><?=$data['content']?></textarea>
+                        </div>
+                    </div>
+                </div>
+                <button type="submit" name="upadate" class="btn btn-primary">update</button>
+            </form>
+        </div>
+    </main>
+</div>
+<!-- /.row -->
---
+++ admin/recources/partials/users/users_table.php
@@ -0,0 +1,84 @@
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-lg-12">
+        <h2 class="page-header">
+            Dashboard / Users
+        </h2>
+    </div>
+    <main>
+        <div class="col-lg-12">
+            <?php
+                // Delete posts form DB
+                if (isset($_GET['user_id'])) {
+                    $id = $_GET['user_id'];
+                    deleteUser($id);
+                }
+            ?>
+            <table class="table table-spride posts_table">
+                <thead>
+                    <tr>
+                        <th>Number</th>
+                        <th>Image</th>
+                        <th>User Name</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    // Get all posts form DB 
+                    $rows = getAllUsers();
+                    $i = 1;
+                    foreach ($rows as $row):
+                        $id = $row['id'];
+                        $image = $row['image'];
+                        $user_name = $row['user_name'];
+                        $first_name = $row['first_name'];
+                        $last_name = $row['last_name'];
+                        $email = $row['email'];
+                        $role = $row['role'];;
+                    ?>
+                    <tr>
+                        <td><?=$i?></td>
+                        <td>
+                            <?php $path = ($image)? "./public/image/$image" : "./public/image/user.png";?>
+                            <img src="<?=$path?>" class="media-object" alt="<?=$user_name?>" />
+                        </td>
+                        <td><?=$user_name?></td>
+                        <td><?=$first_name?></td>
+                        <td><?=$last_name?></td>
+                        <td><?=$email?></td>
+                        <td><?=( $role == 1)? "admin" : "subscriber";?></td>
+                        <td>
+                            <div class="action_dropdown_area">
+                                <div class="dropdown">
+                                    <button type="button" class="btn btn-primary dropdown-toggle"
+                                        data-toggle="dropdown">
+                                        Actions
+                                    </button>
+                                    <div class="dropdown-menu" style="left: -45px;">
+                                        <a href="users.php?flag=edit&id=<?=$id?>" class="dropdown-item">
+                                            <i class="fa fa-pencil info"></i>
+                                            Edit
+                                        </a>
+                                        <a href="users.php?user_id=<?=$id?>" class="dropdown-item">
+                                            <i class="fa fa-trash  danger"></i>
+                                            Delete
+                                        </a>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <?php $i++; endforeach?>
+                </tbody>
+            </table>
+            <!-- Categoroies table -->
+        </div>
+    </main>
+</div>
+<!-- /.row -->
