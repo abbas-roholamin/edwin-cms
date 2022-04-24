41c59e96d680d9c92224e16ab47e98318a0598a7
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-24 23:14:46 +0430

    set profile link in sidebar

--- admin/recources/sidebar.php
+++ admin/recources/sidebar.php
@@ -36,7 +36,7 @@
             </ul>
         </li>
         <li>
-            <a href="index-rtl.html"><i class="fa fa-fw fa-user"></i> Profile</a>
+            <a href="profile.php"><i class="fa fa-fw fa-user"></i> Profile</a>
         </li>
     </ul>
 </div>
