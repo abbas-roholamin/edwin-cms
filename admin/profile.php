b3f2f0ea642e9c011dca43075cc9f74994bbad02
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-24 23:22:05 +0430

    change page header title

--- admin/profile.php
+++ admin/profile.php
@@ -17,7 +17,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <h2 class="page-header">
-                        Dashboard / Edit User
+                        Dashboard / Profile
                     </h2>
                 </div>
                 <main>
