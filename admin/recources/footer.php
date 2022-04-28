834720022e016c89f1f5d006072bd2f444efb172
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-28 10:02:16 +0430

    add ckeditor js file

--- admin/recources/footer.php
+++ admin/recources/footer.php
@@ -4,6 +4,23 @@
 <!-- Bootstrap Core JavaScript -->
 <script src="js/bootstrap.min.js"></script>
 
+<!-- ckeditor plugin -->
+<script src="js/plugins/ckeditor/ckeditor.js"></script>
+<script>
+$(document).ready(function() {
+
+    ClassicEditor
+        .create(document.querySelector('textarea'), {
+            // toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
+        })
+        .then(editor => {
+            window.editor = editor;
+        })
+        .catch(err => {
+            console.error(err.stack);
+        });
+})
+</script>
 </body>
 
 </html>
--- resources/layouts/footer.php
+++ resources/layouts/footer.php
@@ -17,6 +17,24 @@
 <!-- Bootstrap Core JavaScript -->
 <script src="public/js/bootstrap.min.js"></script>
 
+<!-- ckeditor plugin -->
+<script src="admin/js/plugins/ckeditor/ckeditor.js"></script>
+<script>
+$(document).ready(function() {
+
+    ClassicEditor
+        .create(document.querySelector('textarea'), {
+            // toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
+        })
+        .then(editor => {
+            window.editor = editor;
+        })
+        .catch(err => {
+            console.error(err.stack);
+        });
+})
+</script>
+
 </body>
 
 </html>
