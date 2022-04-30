3b6e61e8ac4138f46308602ce716427ca036b208
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-04-30 21:19:28 +0430

    modify registration page

--- registration.php
+++ registration.php
@@ -3,24 +3,23 @@
 
 <!-- Page Content -->
 <div class="container">
-    <div class="login_wrapper">
+    <div class="registration_wrapper">
         <div class="content">
             <h1 class="text-center">Register</h1>
             <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                 <div class="form-group">
                     <label for="username" class="sr-only">User Name</label>
-                    <input type="text" name="username" id="username" class="form-control"
-                        placeholder="Enter Desired Username">
+                    <input type="text" name="username" id="username" class="form-control" placeholder="Enter  Username">
                 </div>
                 <div class="form-group">
                     <label for="username" class="sr-only">First Name</label>
                     <input type="text" name="first_name" id="first_name" class="form-control"
-                        placeholder="Enter Desired First Name">
+                        placeholder="Enter  First Name">
                 </div>
                 <div class="form-group">
                     <label for="username" class="sr-only">Last Name</label>
                     <input type="text" name="last_name" id="last_name" class="form-control"
-                        placeholder="Enter Desired Last Name">
+                        placeholder="Enter  Last Name">
                 </div>
                 <div class="form-group">
                     <label for="email" class="sr-only">Email</label>
