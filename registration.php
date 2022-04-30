0068b90afed73753db3a716740f53d29ab31b62b
Abbas roholamin <92622822+abbas-roholamin@users.noreply.github.com>
2022-05-01 02:11:34 +0430

    create registeration page

--- registration.php
+++ registration.php
@@ -1,36 +1,45 @@
 <!-- Header -->
 <?php include_once("resources/layouts/header.php");?>
-
+<?php
+    if (isset($_POST['registration'])) {
+        $data['user_name'] = $_POST['user_name'];
+        $data['first_name'] = $_POST['first_name'];
+        $data['last_name'] = $_POST['last_name'];
+        $data['email'] = $_POST['email'];
+        $data['password'] = $_POST['password'];
+        $result = registration($data);
+    }
+?>
 <!-- Page Content -->
 <div class="container">
     <div class="registration_wrapper">
         <div class="content">
-            <h1 class="text-center">Register</h1>
+            <h1 class="text-center">Registertion</h1>
             <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                 <div class="form-group">
-                    <label for="username" class="sr-only">User Name</label>
-                    <input type="text" name="username" id="username" class="form-control" placeholder="Enter  Username">
+                    <label for="username">User Name</label>
+                    <input type="text" name="user_name" id="username" class="form-control" placeholder="Enter Username">
                 </div>
                 <div class="form-group">
-                    <label for="username" class="sr-only">First Name</label>
+                    <label for="username">First Name</label>
                     <input type="text" name="first_name" id="first_name" class="form-control"
-                        placeholder="Enter  First Name">
+                        placeholder="Enter First Name">
                 </div>
                 <div class="form-group">
-                    <label for="username" class="sr-only">Last Name</label>
+                    <label for="username">Last Name</label>
                     <input type="text" name="last_name" id="last_name" class="form-control"
-                        placeholder="Enter  Last Name">
+                        placeholder="Enter Last Name">
                 </div>
                 <div class="form-group">
-                    <label for="email" class="sr-only">Email</label>
+                    <label for="email">Email</label>
                     <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
                 </div>
                 <div class="form-group">
-                    <label for="password" class="sr-only">Password</label>
+                    <label for="password">Password</label>
                     <input type="password" name="password" id="key" class="form-control" placeholder="Password">
                 </div>
 
-                <input type="submit" name="submit" id="btn-login" class="btn btn-primary btn-block" value="Register">
+                <input type="submit" name="registration" class="btn btn-primary btn-block" value="Register">
             </form>
         </div>
     </div>
